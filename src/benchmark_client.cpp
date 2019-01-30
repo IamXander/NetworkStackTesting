36b1034e4f28c13232905fa492dbfdf503d357f6
Xander Neben <aneben@ocient.com>
2019-01-30 02:21:31 +0000

    Added more robustness to the test

--- src/benchmark_client.cpp
+++ src/benchmark_client.cpp
@@ -5,10 +5,16 @@
 #include <string>
 
 int main(int argv, char** argc) {
-    if (argv != 2) return 1;
+    if (argv != 5) {
+        std::cout << "Usage: ./client <ip> <port> <packets> <packet_size>" << std::endl;
+        return 1;
+    }
     std::string ip(argc[1]);
+    uint64_t port = atoi(argc[2]);
+    uint64_t packets = atoll(argc[3]);
+    uint64_t packet_size = atoll(argc[4]);
     auto start = std::chrono::system_clock::now();
-    send_data(ip, 8080, 10000000, 1000);
+    send_data(ip, port, packets, packet_size);
     auto end = std::chrono::system_clock::now();
     std::chrono::duration<uint64_t, std::nano> diff = end - start;
     result_t r;
@@ -16,5 +22,13 @@ int main(int argv, char** argc) {
     std::cout << "Nanos: " << r.nanos << std::endl;
     double seconds = (r.nanos / 1e9);
     std::cout << "Seconds: " << seconds << std::endl;
-    std::cout << "Bytes per second: " << (double(10000000) * 1) / seconds << std::endl;
+    double BPS = (double(packets) * double(packet_size)) / seconds;
+    std::cout << "Bytes per second: ";
+    if (BPS >= 1000000000) {
+        std::cout << (BPS / 1000000000) << "GBps" << std::endl;
+    } else if (BPS >= 1000000) {
+        std::cout << (BPS / 1000000) << "MBps" << std::endl;
+    } else {
+        std::cout << BPS << "Bps" << std::endl;
+    }
 }
--- src/benchmark_server.cpp
+++ src/benchmark_server.cpp
@@ -3,6 +3,7 @@
 
 #include <thread>
 #include <atomic>
+#include <iostream>
 
 void cpu_poll_loop(std::atomic<bool>& poll) {
     std::array<core_usage_t, CORE_COUNT> cores;
@@ -11,10 +12,17 @@ void cpu_poll_loop(std::atomic<bool>& poll) {
     }
 }
 
-int main() {
+int main(int argv, char** argc) {
+    if (argv != 4) {
+        std::cout << "Usage: ./server <port> <packets> <packet_size>" << std::endl;
+        return 1;
+    }
+    uint64_t port = atoi(argc[1]);
+    uint64_t packets = atoll(argc[2]);
+    uint64_t packet_size = atoll(argc[3]);
     std::atomic<bool> poll(true);
     std::thread poll_thread(cpu_poll_loop, std::ref(poll));
-    recv_data(8080, 10000000, 1000);
+    recv_data(port, packets, packet_size);
     poll = false;
     poll_thread.join();
 }
